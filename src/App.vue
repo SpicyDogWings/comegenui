3e31abb342afbb7bf39c3653e1a318050aeef39f
SpicyDogWings <spaceprotondev@proton.me>
2026-04-24 21:42:59 -0600

    feat: soft variant agregada

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import Button from "./components/Button.ce.vue";
 
 <template>
   <div class="w-screen h-screen flex justify-center items-center gap-1">
-    <Button color="primary" variant="outlined">Priamry</Button>
-    <Button color="neutral" variant="outlined">Neutral</Button>
-    <Button color="success" variant="outlined">Success</Button>
-    <Button color="warning" variant="outlined">Warning</Button>
-    <Button color="danger" variant="outlined">Danger</Button>
+    <Button color="primary" variant="soft">Priamry</Button>
+    <Button color="neutral" variant="soft">Neutral</Button>
+    <Button color="success" variant="soft">Success</Button>
+    <Button color="warning" variant="soft">Warning</Button>
+    <Button color="danger" variant="soft">Danger</Button>
   </div>
 </template>
--- src/components/Button.ce.vue
+++ src/components/Button.ce.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     type: String,
     required: false,
     default: "solid",
-    validator: (value: string) => ["solid", "outlined"].includes(value),
+    validator: (value: string) => ["solid", "outlined", "soft"].includes(value),
   },
 });
 </script>
@@ -21,16 +21,16 @@ const props = defineProps({
     class="py-2 px-4 rounded-cu border-none font-sans font-bold hover:cursor-pointer"
     :class="[
       {
-        'bg-charcoal-50 text-charcoal hover:bg-charcoal-100 active:bg-charcoal-200':
-          !props.color && props.variant === 'solid',
-      },
-      {
         'bg-transparent border-solid border-2 border-charcoal text-charcoal':
           !props.color && props.variant === 'outlined',
       },
       {
         'bg-primary text-primary-50 hover:bg-primary-600 active:bg-primary-700':
           props.color === 'primary' && props.variant === 'solid',
+      },
+      {
+        'bg-primary-100 text-primary hover:bg-primary-200 active:bg-primary-300':
+          props.color === 'primary' && props.variant === 'soft',
       },
       {
         'bg-transparent border-solid border-2 border-primary text-primary':
@@ -41,12 +41,20 @@ const props = defineProps({
           props.color === 'neutral' && props.variant === 'solid',
       },
       {
+        'bg-charcoal-100 text-charcoal-800 hover:bg-charcoal-200 active:bg-charcoal-300':
+          props.color === 'neutral' && props.variant === 'soft',
+      },
+      {
         'bg-transparent border-solid border-2 border-charcoal-800 text-charcoal-800':
           props.color === 'neutral' && props.variant === 'outlined',
       },
       {
         'bg-success text-success-50 hover:bg-success-500 active:bg-success-600':
           props.color === 'success' && props.variant === 'solid',
+      },
+      {
+        'bg-success-100 text-success-600 hover:bg-success-200 active:bg-success-300':
+          props.color === 'success' && props.variant === 'soft',
       },
       {
         'bg-transparent border-solid border-2 border-success text-success':
@@ -57,12 +65,20 @@ const props = defineProps({
           props.color === 'warning' && props.variant === 'solid',
       },
       {
+        'bg-warning-100 text-warning-500 hover:bg-warning-200 active:bg-warning-300':
+          props.color === 'warning' && props.variant === 'soft',
+      },
+      {
         'bg-transparent border-solid border-2 border-warning text-warning':
           props.color === 'warning' && props.variant === 'outlined',
       },
       {
         'bg-danger text-danger-50 hover:bg-danger-700 active:bg-danger-800':
           props.color === 'danger' && props.variant === 'solid',
+      },
+      {
+        'bg-danger-100 text-danger hover:bg-danger-200 active:bg-danger-300':
+          props.color === 'danger' && props.variant === 'soft',
       },
       {
         'bg-transparent border-solid border-2 border-danger text-danger':
