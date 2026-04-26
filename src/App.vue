6e6d72ba29c6e7dea43b7024318eed89c2c7090e
SpicyDogWings <spaceprotondev@proton.me>
2026-04-25 22:04:19 -0600

    ui: layout de test de badge

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,13 @@ import Badge from "./components/Badge.ce.vue";
 </script>
 
 <template>
-  <div class="w-screen h-screen flex flex-col justify-center items-center gap-4">
+  <div
+    class="w-screen h-screen flex flex-col justify-center items-center gap-4"
+  >
     <div class="flex gap-1">
-      <Button color="primary" variant="solid">Primary</Button>
-      <Button color="neutral" variant="solid">Neutral</Button>
-      <Button color="success" variant="solid">Success</Button>
-      <Button color="warning" variant="solid">Warning</Button>
-      <Button color="danger" variant="solid">Danger</Button>
-    </div>
-    <div class="flex gap-1">
-      <Badge color="primary" variant="solid">New</Badge>
-      <Badge color="neutral" variant="soft">12</Badge>
-      <Badge color="success" variant="subtle">Online</Badge>
-      <Badge color="warning" variant="outlined">Pending</Badge>
-      <Badge color="danger" variant="ghost">Urgent</Badge>
+      <Badge color="primary" variant="solid">
+        Terminal
+      </Badge>
     </div>
   </div>
 </template>
