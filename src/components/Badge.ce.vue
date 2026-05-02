7c6b591c2194f9cd26af37864a538103b4b214df
Jocsan Fonseca <jocsan.fonseca@di.unanleon.edu.ni>
2026-05-02 16:36:51 -0600

    refactor: optimización de clases en badge

--- src/components/Badge.ce.vue
+++ src/components/Badge.ce.vue
@@ -19,24 +19,40 @@ const props = defineProps({
 });
 
 const badgeClasses = computed(() => [
+  /* Regla general para soft */
+  {
+    "bg-opacity-10": props.variant === "soft",
+  },
+  /* Regla general para subtle */
+  {
+    "bg-opacity-10 border-solid border-1": props.variant === "subtle",
+  },
+  /* Regla general para outlined */
+  {
+    "bg-transparent border-solid border-2": props.variant === "outlined",
+  },
+  /* Regla general para ghost */
+  {
+    "bg-transparent hover:bg-opacity-10": props.variant === "ghost",
+  },
   {
     "bg-primary text-primary-50":
       props.color === "primary" && props.variant === "solid",
   },
   {
-    "bg-primary bg-opacity-10 text-primary":
+    "bg-primary text-primary":
       props.color === "primary" && props.variant === "soft",
   },
   {
-    "bg-primary bg-opacity-10 text-primary border-solid border-1 border-primary-300":
+    "bg-primary text-primary border-primary-300":
       props.color === "primary" && props.variant === "subtle",
   },
   {
-    "bg-transparent border-solid border-2 border-primary text-primary":
+    "border-primary text-primary":
       props.color === "primary" && props.variant === "outlined",
   },
   {
-    "bg-transparent text-primary hover:bg-primary hover:bg-opacity-10":
+    "text-primary hover:bg-primary":
       props.color === "primary" && props.variant === "ghost",
   },
   {
@@ -44,19 +60,19 @@ const badgeClasses = computed(() => [
       props.color === "neutral" && props.variant === "solid",
   },
   {
-    "bg-charcoal bg-opacity-10 text-charcoal-800":
+    "bg-charcoal text-charcoal-800":
       props.color === "neutral" && props.variant === "soft",
   },
   {
-    "bg-charcoal bg-opacity-10 text-charcoal-800 border-solid border-1 border-charcoal-300":
+    "bg-charcoal text-charcoal-800 border-charcoal-300":
       props.color === "neutral" && props.variant === "subtle",
   },
   {
-    "bg-transparent border-solid border-2 border-charcoal-800 text-charcoal-800":
+    "border-charcoal-800 text-charcoal-800":
       props.color === "neutral" && props.variant === "outlined",
   },
   {
-    "bg-transparent text-charcoal-800 hover:bg-charcoal hover:bg-opacity-10":
+    "text-charcoal-800 hover:bg-charcoal":
       props.color === "neutral" && props.variant === "ghost",
   },
   {
@@ -64,19 +80,19 @@ const badgeClasses = computed(() => [
       props.color === "success" && props.variant === "solid",
   },
   {
-    "bg-success bg-opacity-10 text-success-600":
+    "bg-success text-success-600":
       props.color === "success" && props.variant === "soft",
   },
   {
-    "bg-success bg-opacity-10 text-success-600 border-solid border-1 border-success-300":
+    "bg-success text-success-600 border-success-300":
       props.color === "success" && props.variant === "subtle",
   },
   {
-    "bg-transparent border-solid border-2 border-success text-success":
+    "border-success text-success":
       props.color === "success" && props.variant === "outlined",
   },
   {
-    "bg-transparent text-success-600 hover:bg-success hover:bg-opacity-10":
+    "text-success-600 hover:bg-success":
       props.color === "success" && props.variant === "ghost",
   },
   {
@@ -84,19 +100,19 @@ const badgeClasses = computed(() => [
       props.color === "warning" && props.variant === "solid",
   },
   {
-    "bg-warning bg-opacity-10 text-warning-500":
+    "bg-warning text-warning-500":
       props.color === "warning" && props.variant === "soft",
   },
   {
-    "bg-warning bg-opacity-10 text-warning-500 border-solid border-1 border-warning-300":
+    "bg-warning text-warning-500 border-warning-300":
       props.color === "warning" && props.variant === "subtle",
   },
   {
-    "bg-transparent border-solid border-2 border-warning text-warning":
+    "border-warning text-warning":
       props.color === "warning" && props.variant === "outlined",
   },
   {
-    "bg-transparent text-warning-500 hover:bg-warning hover:bg-opacity-10":
+    "text-warning-500 hover:bg-warning":
       props.color === "warning" && props.variant === "ghost",
   },
   {
@@ -104,19 +120,19 @@ const badgeClasses = computed(() => [
       props.color === "danger" && props.variant === "solid",
   },
   {
-    "bg-danger bg-opacity-10 text-danger":
+    "bg-danger text-danger":
       props.color === "danger" && props.variant === "soft",
   },
   {
-    "bg-danger bg-opacity-10 text-danger border-solid border-1 border-danger-300":
+    "bg-danger text-danger border-danger-300":
       props.color === "danger" && props.variant === "subtle",
   },
   {
-    "bg-transparent border-solid border-2 border-danger text-danger":
+    "border-danger text-danger":
       props.color === "danger" && props.variant === "outlined",
   },
   {
-    "bg-transparent text-danger hover:bg-danger hover:bg-opacity-10":
+    "text-danger hover:bg-danger":
       props.color === "danger" && props.variant === "ghost",
   },
 ]);
@@ -124,7 +140,7 @@ const badgeClasses = computed(() => [
 
 <template>
   <span
-    class="py-0.5 px-2 rounded-cu border-none font-sans font-medium font-size-3 flex justify-center items-center gap-2 box-border"
+    class="py-0.5 px-2 w-fit rounded-cu border-none font-sans font-medium font-size-3 flex justify-center items-center gap-2 box-border"
     :class="badgeClasses"
   >
     <slot></slot>
