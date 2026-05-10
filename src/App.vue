bb8167b8f4763236de673fe58ef6cd85aa20d745
SpicyDogWings <spaceprotondev@proton.me>
2026-05-10 14:14:36 -0600

    fix: los colores del estilo link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Button from "./components/Button.vue";
-const color = "#093C5D";
+const color = "#59B292";
 </script>
 
 <template>
@@ -9,7 +9,7 @@ const color = "#093C5D";
     <Button variant="outlined" :color="color">Click me</Button>
     <Button variant="soft" :color="color">Click me</Button>
     <Button :color="color">Click me</Button>
-    <Button variant="subtle" :color="color">Click me</Button>
+    <Button variant="link" to="https://example.com" :color="color">Click me</Button>
   </div>
 </template>
 
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -40,8 +40,12 @@ const props = defineProps({
   },
 });
 
-const bgClass = computed(() => getBgClasses(props.color, props.variant, props.hightContrast));
-const fgClass = computed(() => getFgClasses(props.color, props.variant, props.hightContrast));
+const bgClass = computed(() =>
+  getBgClasses(props.color, props.variant, props.hightContrast),
+);
+const fgClass = computed(() =>
+  getFgClasses(props.color, props.variant, props.hightContrast),
+);
 </script>
 
 <template>
@@ -53,16 +57,26 @@ const fgClass = computed(() => getFgClasses(props.color, props.variant, props.hi
       'decoration-0': props.variant !== 'link',
       'visited:text-[var(--btn-fg)]': props.variant === 'link',
       'cursor-not-allowed opacity-70': props.disabled,
-
+    }"
+    :style="{
+      '--btn-fg': fgClass.main,
     }"
   >
     <button
       class="py-2 px-4 rounded-cu border-none text-[var(--btn-fg)] font-sans font-medium bg-[var(--btn-bg)] flex justify-center items-center gap-2 box-border"
       :class="{
-        'hover:underline hover:decoration-solid hover:decoration-2': props.variant === 'link',
+        'hover:underline hover:decoration-solid hover:decoration-2 visited:text-[var(--btn-fg)]':
+          props.variant === 'link',
         'cursor-not-allowed opacity-70': props.disabled,
-        'active:bg-[var(--btn-bg-active)] hover:cursor-pointer': !props.disabled,
-
+        'active:bg-[var(--btn-bg-active)] hover:cursor-pointer':
+          !props.disabled,
+      }"
+      :style="{
+        '--btn-fg': fgClass.main,
+        '--btn-bg': bgClass.main,
+        '--btn-bg-hover': bgClass.hover,
+        '--btn-bg-active': bgClass.active,
+        '--btn-bd': fgClass.border,
       }"
       :disabled="props.disabled"
     >
@@ -73,10 +87,13 @@ const fgClass = computed(() => getFgClasses(props.color, props.variant, props.hi
     v-else
     class="py-2 px-4 rounded-cu border-none text-[var(--btn-fg)] font-sans font-medium bg-[var(--btn-bg)] flex justify-center items-center gap-2 box-border"
     :class="{
-      'border-1 border-solid border-[var(--btn-bd)]': props.variant === 'outlined' || props.variant === 'subtle',
-      'hover:underline hover:decoration-solid hover:decoration-2': props.variant === 'link',
+      'border-1 border-solid border-[var(--btn-bd)]':
+        props.variant === 'outlined' || props.variant === 'subtle',
+      'hover:underline hover:decoration-solid hover:decoration-2':
+        props.variant === 'link',
       'cursor-not-allowed opacity-70': props.disabled,
-      'hover:bg-[var(--btn-bg-hover)] active:bg-[var(--btn-bg-active)] hover:cursor-pointer': !props.disabled,
+      'hover:bg-[var(--btn-bg-hover)] active:bg-[var(--btn-bg-active)] hover:cursor-pointer':
+        !props.disabled,
     }"
     :style="{
       '--btn-fg': fgClass.main,
