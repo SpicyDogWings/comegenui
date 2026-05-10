0926634344f44453031f2c99d25d2737745a9595
SpicyDogWings <spaceprotondev@proton.me>
2026-05-10 10:43:26 -0600

    feat: adjust fg contrast threshold based on hightContrast prop

--- src/utils/palette.ts
+++ src/utils/palette.ts
@@ -31,7 +31,7 @@ const getBgClasses = (color: string, variant: string, hightContrast: boolean) =>
 const getFgClasses = (color: string, variant: string, hightContrast: boolean) => {
   let main = toHex(lighten(color, 0.6));
   const contrast = getContrast(main, color);
-  if (contrast < 3) main = toHex(darken(color, hightContrast ? 0.7 : 0.5));
+  if (hightContrast ? contrast < 3 : contrast < 2) main = toHex(darken(color, hightContrast ? 0.7 : 0.5));
   if (["ghost", "outlined", "soft", "subtle", "link"].includes(variant)) main = color;
   let border = "";
   if (variant === "subtle") border = transparentize(color, 0.7);
