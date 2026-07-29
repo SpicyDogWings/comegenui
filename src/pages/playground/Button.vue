8c0ee53ab832e8eb11b3e728b7a790a9a0b5844c
unijfdev <jocsan.fonseca@di.unanleon.edu.ni>
2026-07-29 11:31:09 -0600

    chore: add missing files (vars.css, playground pages)

---
+++ src/pages/playground/Button.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Button from "@/components/Button.vue"
+</script>
+
+<template>
+    <section class="playground">
+        <div class="playground-headerbar">
+            <h1>Playground</h1>
+            <Button>Light</Button>
+        </div>
+        <div class="playground-box">
+            <Button variant="subtle">
+                Haz click
+            </Button>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.playground {
+    width: 100dvw;
+    height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--playground-bg);
+}
+
+.playground-headerbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 2rem 0 2rem;
+}
+
+.playground-box {
+    width: 100dvw;
+    height: 100dvh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
