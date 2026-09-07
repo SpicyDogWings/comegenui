33584417b0909622b6290c5088da34fb5867ee25
SpicyDogWings <spaceprotondev@proton.me>
2026-09-06 18:40:42 -0600

    fix(markdown): variante default para previews de CodeBlock
    Cambia variant de solid a default en los bloques de código
    renderizados por el componente Markdown.

--- src/components/markdown/Markdown.vue
+++ src/components/markdown/Markdown.vue
@@ -106,7 +106,7 @@ defineExpose({
           v-else-if="block.type === 'code-block' && block.codeBlock"
           :code="block.codeBlock.code"
           :language="block.codeBlock.language"
-          variant="solid"
+          variant="default"
         />
         <Blockquote
           v-else-if="block.type === 'blockquote' && block.blockquote"
